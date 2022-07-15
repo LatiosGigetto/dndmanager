ccd97e6eb84a375ee2891fd198da1de6af93f535
amir-othmani <amir.othmani@hotmail.it>
2022-07-15 11:46:50 +0200

    Propototipo Master

---
+++ ManagerCodice/Controller/Master.py
@@ -0,0 +1,38 @@
+
+
+class Master:
+
+    def aggiornaScheda(self):
+        print(":p")
+
+    def aggiornaPersonaggi(self):
+        print(":p")
+
+    def creaAppunti(self):
+        print(":p")
+
+    def cambioCredenziali(self):
+        print(":p")
+
+    def creaStatistiche(self):
+        print(":p")
+
+    def eliminaAppunti(self):
+        print(":p")
+
+    def gestisciContatore(self):
+        print(":p")
+
+    def pubblicaAppunti(self):
+        print(":p")
+
+    def tiraDado(self):
+        print(":p")
+
+    def visualizzaAppunti(self):
+        print(":p")
+
+    def visualizzaPersonaggi(self):
+        print(":p")
+
+
