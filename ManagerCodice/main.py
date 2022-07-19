3caa7b8f0a45ced4044700427285f6104da8a83b
Alessandro Rossini <latiosgigetto@gmail.com>
2022-07-19 10:14:55 +0200

    main

--- ManagerCodice/main.py
+++ ManagerCodice/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import sys
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from View import VistaAccesso
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
+if __name__ == "__main__":
+    import sys
+    vistaAccesso = VistaAccesso
+    app = QApplication(sys.argv)
+    Login = QMainWindow()
+    ui = VistaAccesso.Ui_Login()
+    ui.setupUi(Login)
+    Login.show()
+    sys.exit(app.exec_())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
